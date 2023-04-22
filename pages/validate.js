4da8ecd49ec57753a209e31a71030f78a443e5e4
Ara Ventura <ara.venturafitch@gmail.com>
2023-04-22 15:03:17 -0700

    fixed bug on event handler to select inputs from form

--- pages/validate.js
+++ pages/validate.js
@@ -11,7 +11,7 @@ export const enableValidation = (data) => {
 
 //this function will start validation 
 const setupValidationEventListeners = (formElement, popupInputSelector) => {
-    const inputList = Array.from(document.querySelectorAll(popupInputSelector));
+    const inputList = Array.from(formElement.querySelectorAll(popupInputSelector));
     inputList.forEach((inputElement) => {
         inputElement.addEventListener("input", () => {
             checkInputValidity(inputElement, formElement);
@@ -31,10 +31,10 @@ const showInputError = (inputElement, formElement) => {
     const inputWrapper = formElement.querySelector(`#${inputElement.id}-wrapper`);
     const errorElement = inputWrapper.querySelector(`.${inputElement.id}__error-message`);
     inputElement.classList.add("popup__input_error_invalid");
-
-    errorElement.textContent = inputElement.errorMessage;
+    errorElement.textContent = inputElement.validationMessage;
+    console.log(inputElement.errorMessage);
     errorElement.classList.add("popup__error-message_type_active");
-    
+
     //aqui agregar para hacer el boton inactivo
 }
 
@@ -43,7 +43,7 @@ const hideInputError = (inputElement, formElement) => {
     const inputWrapper = formElement.querySelector(`#${inputElement.id}-wrapper`);
     console.log("yiyo", formElement)
     const errorElement = inputWrapper?.querySelector(`.${inputElement.id}__error-message`);
-    errorElement?.classList.remove("popup__error-message_type_active");
+    errorElement.classList.remove("popup__error-message_type_active");
     inputElement.classList.remove("popup__input_error_invalid");
 }
 
