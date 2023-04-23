f3c2bcd206a8604930294e3b1f4fdbe8a87d26df
Ara Ventura <ara.venturafitch@gmail.com>
2023-04-22 19:27:03 -0700

    add togleformbutton function and reset input validation

--- pages/validate.js
+++ pages/validate.js
@@ -22,8 +22,10 @@ const setupValidationEventListeners = (formElement, popupInputSelector) => {
 const checkInputValidity = (inputElement, formElement) => {
     if (!inputElement.validity.valid) {
         showInputError(inputElement, formElement)
+        toggleFormButton(formElement); // this function toggles button, prevents false entries
     } else {
         hideInputError(inputElement, formElement);
+        toggleFormButton(formElement); //function that toggles button
     }
 }
 
@@ -32,18 +34,41 @@ const showInputError = (inputElement, formElement) => {
     const errorElement = inputWrapper.querySelector(`.${inputElement.id}__error-message`);
     inputElement.classList.add("popup__input_error_invalid");
     errorElement.textContent = inputElement.validationMessage;
-    console.log(inputElement.errorMessage);
     errorElement.classList.add("popup__error-message_type_active");
-
-    //aqui agregar para hacer el boton inactivo
 }
 
+export const toggleFormButton = (formElement) => {
+    const popupInputs = Array.from(formElement.querySelectorAll(".popup__input-wrapper .popup__input"));
+    if (!popupInputs[0].validity.valid || !popupInputs[1].validity.valid) {
+        const formButton = formElement.querySelector(".popup__button");
+        formButton.setAttribute("disabled", true);
+        formButton.classList.add("popup__button-inactive");
+    } else {
+        const formButton = formElement.querySelector(".popup__button");
+        formButton.removeAttribute("disabled");
+        formButton.classList.remove("popup__button-inactive");
+    }
+}
+
+
 const hideInputError = (inputElement, formElement) => {
-    console.log(inputElement.id);
     const inputWrapper = formElement.querySelector(`#${inputElement.id}-wrapper`);
-    console.log("yiyo", formElement)
-    const errorElement = inputWrapper?.querySelector(`.${inputElement.id}__error-message`);
+    const errorElement = inputWrapper.querySelector(`.${inputElement.id}__error-message`);
     errorElement.classList.remove("popup__error-message_type_active");
+    errorElement.classList.add("popup__error-message_type_inactive")
     inputElement.classList.remove("popup__input_error_invalid");
 }
 
+export const resetInputValidation = (popup) => {
+    const inputList = Array.from(popup.querySelectorAll(".popup .popup__container .form .popup__input-wrapper .popup__input"));
+    const errorMessages = Array.from(popup.querySelectorAll(".popup .popup__container .form .popup__input-wrapper span"));
+    errorMessages.forEach((message) => {
+        message.classList.remove("popup__error-message_type_active");
+        message.classList.add("popup__error-message_type_inactive");
+        message.textContent = "";
+    })
+
+    inputList.forEach((input) => {
+        input.classList.remove("popup__input_error_invalid");
+    })
+}
