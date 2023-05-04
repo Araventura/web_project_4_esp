82bae84b120b26eac0bb65e2c09c38550c5c567d
Daniel Villaverde <j.danielv@outlook.com>
2023-05-03 20:47:18 -0700

    create additional JS files

---
+++ pages/Card.js
@@ -0,0 +1,6 @@
+// all info of card should be exported to index.js
+//produce una tarjeta y enlace a la imagen, aqui se produce la card??
+
+const Card = (text, url, clase) {
+    
+}
---
+++ pages/utils.js
@@ -0,0 +1,2 @@
+//controladores de eventos & funcion que abre &cierra las ventanas modales se exportan
+//openAddPopup, openEdit, closeAdd, close Edit
