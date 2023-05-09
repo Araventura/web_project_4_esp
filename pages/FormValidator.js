443cceba84823360e2b419153e5a33209c4bac8f
Daniel Villaverde <j.danielv@outlook.com>
2023-05-08 19:21:08 -0700

    create formValidator file

--- pages/FormValidator.js
+++ pages/FormValidator.js
@@ -1,1 +1,4 @@
 //export form for FormValidator
+//establece la configuracion para validar los campos. Este es un constructor
+//dos parametros 1) objeto que almacena a los selectores
+//2)toma un elemnto del formulario a validad
